189df69bb5c45fd2aba230f071a9d334f5fb5560
Ostap <Ostapkriven13@gmail.com>
2023-06-20 21:33:52 +0300

    і
    і

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const CARDS = 10;
 const MAX_VISIBILITY = 3;
 
-const Card = ({ title, content }) => {
+const createCard = ({ title, content }) => {
   const card = document.createElement('div');
   card.className = 'card';
   card.innerHTML = `
@@ -11,7 +11,7 @@ const Card = ({ title, content }) => {
   return card;
 };
 
-const Carousel = ({ children }) => {
+const createCarousel = () => {
   const carousel = document.createElement('div');
   carousel.className = 'carousel';
   const navLeft = document.createElement('button');
@@ -20,20 +20,27 @@ const Carousel = ({ children }) => {
   const navRight = document.createElement('button');
   navRight.className = 'nav right';
   navRight.innerHTML = '<svg>...</svg>';
-  const cards = Array.from(children);
+  const cards = Array.from({ length: CARDS }, (_, i) =>
+    createCard({
+      title: 'Card ' + (i + 1),
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    })
+  );
 
   let active = 2;
 
   const updateCards = () => {
     cards.forEach((card, i) => {
+      const offset = (active - i) / 3;
+      const absOffset = Math.abs(active - i) / 3;
+      const direction = Math.sign(active - i);
       const cardContainer = card.querySelector('.card-container');
-      cardContainer.style.setProperty('--active', i === active ? '1' : '0');
-      cardContainer.style.setProperty('--offset', (active - i) / 3);
-      cardContainer.style.setProperty('--direction', Math.sign(active - i));
-      cardContainer.style.setProperty('--abs-offset', Math.abs(active - i) / 3);
-      cardContainer.style.setProperty('pointer-events', active === i ? 'auto' : 'none');
-      cardContainer.style.setProperty('opacity', Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1');
-      cardContainer.style.setProperty('display', Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block');
+      cardContainer.style.transform = `rotateY(calc(${offset} * 50deg)) scaleY(calc(1 + ${absOffset} * -0.4)) translateZ(calc(${absOffset} * -30rem)) translateX(calc(${direction} * -5rem))`;
+      cardContainer.style.filter = `blur(calc(${absOffset} * 1rem))`;
+      cardContainer.style.pointerEvents = active === i ? 'auto' : 'none';
+      cardContainer.style.opacity = Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1';
+      cardContainer.style.display = Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block';
     });
   };
 
@@ -56,22 +63,5 @@ const Carousel = ({ children }) => {
   return carousel;
 };
 
-const App = () => {
-  const carousel = document.createElement('div');
-  carousel.className = 'app';
-  const carouselComponent = new Carousel();
-  const cardComponents = Array.from(new Array(CARDS), (_, i) =>
-    new Card({
-      title: 'Card ' + (i + 1),
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    })
-  );
-
-  carouselComponent.append(...cardComponents);
-  carousel.append(carouselComponent);
-
-  return carousel;
-};
-
-document.body.appendChild(App());
+const app = document.getElementById('app');
+app.appendChild(createCarousel());
